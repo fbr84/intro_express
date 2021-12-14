e9e01539bf313e4a2cdbeb68caa3d837a5ac1e37
Federico Bravo <fedebravo84@gmail.com>
2021-12-14 14:59:37 +0100

    Hello world

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`)

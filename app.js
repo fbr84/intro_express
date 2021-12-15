c53a72a73bd82d2600ec883fc18e463b46de106c
Federico Bravo <fedebravo84@gmail.com>
2021-12-15 14:53:40 +0100

    middleware

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  // res.send('<h1>INICIO</h1>')
+  res.sendFile('./views/index.html', {root:__dirname})
 })
+
+app.get('/about', (req, res) => {
+  // res.send('<h1>Sobre Nosotros</h1>')
+  res.sendFile('./views/about.html', {root:__dirname})
+})
+
+app.get('/about-us',(req,res) => {
+  res.redirect('/about')
+})
+
+app.use((req, res) => {
+  res.sendFile('./views/404.html', {root:__dirname})
+})
+
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`)
